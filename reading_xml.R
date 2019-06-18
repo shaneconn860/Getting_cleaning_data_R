f5db0d877f64f62a270edadf02a884de1c1831eb
Shane Connaughton <shaneconn86@gmail.com>
2019-06-18 12:55:50 +0100

    First commit

---
+++ reading_xml.R
@@ -0,0 +1,20 @@
+#Load XML package in to R
+library(XML)
+
+
+#Read the file in to R
+fileUrl <- "http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+doc <- xmlTreeParse(fileUrl, useInternal=TRUE)
+rootNode <- xmlRoot(doc)
+xmlName(rootNode)
+
+
+#Inspect nodes of the XML document
+rootNode[[1]]
+rootNode[[1]][[1]]
+rootNode[[1]][[2]]
+
+
+#Return each specific value of a node
+xpathSApply(rootNode, "//name", xmlValue)
+xpathSApply(rootNode, "//policedistrict", xmlValue)
