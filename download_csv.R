ffac24413e6415c717924a21c2c55d8a7079b02d
Shane Connaughton <shaneconn86@gmail.com>
2019-06-18 12:43:54 +0100

    Added some commands as comments for inspecting the data. Also changed download url.

--- download_csv.R
+++ download_csv.R
@@ -9,7 +9,18 @@ if (!file.exists("./data4")) {
 
 #Download file
         
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+fileUrl <- "https://data.baltimorecity.gov/api/views/k5ry-ef3g/rows.csv?accessType=DOWNLOAD"
 download.file(fileUrl, destfile = "./data4/test.csv")
 camdata <- read.csv("./data4/test.csv")
 }
+
+#summary(camdata)
+#str(camdata)
+#table(camdata$councilDistrict, camdata$zipCode)
+#sum(is.na(camdata$councilDistrict))
+#any(is.na(camdata$councilDistrict))
+#all(camdata$zipCode > 0)
+#colSums(is.na(camdata))
+#all(colSums(is.na(camdata))==0)
+#table(camdata$zipCode %in% c("21212", "21213"))
+#camdata[camdata$zipCode %in% c("21212", "21213"),] - subset list of restaurants in Zipcodes
